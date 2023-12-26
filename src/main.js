b41a6a149b0617873d52170780236a910a2436ae
YuichiCanete <142189950+YuichiCanete@users.noreply.github.com>
2023-12-26 13:06:34 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
-
-createApp(App).mount('#app')
