8ed3e06e6d951470b5e5665b0e7771090cd1043e
YuichiCanete <142189950+YuichiCanete@users.noreply.github.com>
2023-12-26 13:04:20 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'primevue/resources/themes/lara-light-pink/theme.css'
+import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
-import router from "./router";
-
-
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).mount('#app')
