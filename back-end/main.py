b568e1ff38361da92ec71ca2d63499b5858cd525
YuichiCanete <ycanete_220000000743@uic.edu.ph>
2024-04-21 08:31:20 +0800

    working fastapi

--- back-end/main.py
+++ back-end/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from model.routes import router
+from model.user import userRouter
+from model.food_details import foodDetailsRouter
+from model.user_order import userOrderRouter
+from model.food import foodRouter
+from model.order import orderRouter
+from model.tally import tallyRouter
 
 app = FastAPI()
 
 # Include CRUD routes from users module
-app.include_router(router, prefix="/api")
+app.include_router(userRouter, prefix="/api")
+app.include_router(foodDetailsRouter, prefix="/api")
+app.include_router(userOrderRouter, prefix="/api")
+app.include_router(foodRouter, prefix="/api")
+app.include_router(orderRouter, prefix="/api")
+app.include_router(tallyRouter, prefix="/api")
 
 # Configure CORS settings
 app.add_middleware(
--- back-end/model/user.py
+++ back-end/model/user.py
@@ -1,19 +1,18 @@
-from fastapi import Depends, HTTPException, APIRouter, Form
+from fastapi import Depends, APIRouter, Form
 from .db import get_db
-from datetime import date
 from .fn import fetch_data, delete_data, create_data, update_data
 
-router = APIRouter()
+userRouter = APIRouter(tags=['User'])
 
-@router.get("/users")
+@userRouter.get("/users")
 async def get_users(db=Depends(get_db)):
     return await fetch_data("user",db=db)
 
-@router.get("/users/{user_id}")
+@userRouter.get("/users/{user_id}")
 async def get_user_by_id(user_id: int,db=Depends(get_db)):
     return await fetch_data("user", condition="user_id", value=user_id,db=db)
 
-@router.post("/users/", response_model=dict)
+@userRouter.post("/users/", response_model=dict)
 async def create_user(
     user_id: int = Form(...),
     password: str = Form(...),
@@ -27,11 +26,11 @@ async def create_user(
     }
     return await create_data("user", data, db=db)
 
-@router.delete("/users/{user_id}", response_model=dict)
+@userRouter.delete("/users/{user_id}", response_model=dict)
 async def delete_user(user_id: int, db=Depends(get_db)):
     return await delete_data("user", "user_id", user_id, db=db)
 
-@router.put("/users/{user_id}", response_model=dict)
+@userRouter.put("/users/{user_id}", response_model=dict)
 async def update_user(
     user_id: int,
     password: str = Form(...),
